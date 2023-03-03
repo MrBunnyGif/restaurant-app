4db67648316fb997c44df57a48958c802c3a961b
Pedro <botegapedro@gmail.com>
2023-03-03 19:00:41 -0300

    Updates App with initial login screen components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LoginForm from './Components/LoginForm';
+import SystemLogo from './Components/SystemLogo';
 
 function App() {
   return (
-    <div className="App">
+    <div className="full-screen-container">
+      <LoginForm />
+      <SystemLogo />
     </div>
   );
 }
