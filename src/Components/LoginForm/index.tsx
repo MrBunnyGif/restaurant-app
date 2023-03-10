e0cf6523e80fb36acf064ee689e39fcde38c9acf
Pedro <botegapedro@gmail.com>
2023-03-10 11:50:03 -0300

    Fixes LoginForm elements positions

--- src/Components/LoginForm/index.tsx
+++ src/Components/LoginForm/index.tsx
@@ -4,15 +4,22 @@ import Buton from "../Buton"
 
 export default () => {
     return (
-        <div>
-            <img src={logo} className="App-logo" alt="logo" />
+        <div style={{
+            position: 'fixed',
+            top: '50%',
+            left: '0',
+            right: '0',
+            transform: 'translate(0%, calc(-50% - 75px))',
+            textAlign:'center'
+        }}>
+            <img width={200} src={logo} className="App-logo" alt="logo" />
             <div style={{
                 display: 'flex',
                 flexDirection: 'column',
                 maxWidth: '70%',
                 margin: 'auto'
             }}>
-                <Input placeholder="email"  />
+                <Input placeholder="email" />
                 <Input placeholder="password" />
                 <Buton />
             </div>
--- src/Components/SystemLogo/index.tsx
+++ src/Components/SystemLogo/index.tsx
@@ -1,7 +1,13 @@
 import logo from '../../logo.svg'
 
 export default () => (
-    <footer>
-        <img src={logo} className="App-logo" alt="logo" />
+    <footer style={{
+        position: 'fixed',
+        bottom: '0',
+        left: '0',
+        right: '0',
+        textAlign:'center'
+    }}>
+        <img width={100} src={logo} className="App-logo" alt="logo" />
     </footer>
 )
