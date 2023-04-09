e33f09707c56b45111510e2ddcfabe7080cb1f3d
Pedro <botegapedro@gmail.com>
2023-04-08 21:42:22 -0300

    Fix  BascketFooter component identation

--- src/Components/BascketFooter/index.tsx
+++ src/Components/BascketFooter/index.tsx
@@ -1,27 +1,29 @@
 import { divType } from '../../types/Components'
 import Button from '../Button'
 
-export default (props: divType) => (
-    <footer style={{
-        backgroundColor: 'tomato',
-        position: 'fixed',
-        bottom: '0',
-        right: '0',
-        left: '0',
-        padding: '1rem 0.5rem',
-        display: 'flex',
-        justifyContent: 'space-between'
-    }} onClick={props.onClick}>
-        <div>
-            {props.inBascket ?
-                <></>
-                :
-                <span style={{
-                    marginRight: '1rem'
-                }}>N° Items</span>
-            }
-            <span>R$ Total</span>
-        </div>
-        <Button />
-    </footer >
-)
+export default (props: divType) => {
+	return (
+		<footer style={{
+			backgroundColor: 'tomato',
+			position: 'fixed',
+			bottom: '0',
+			right: '0',
+			left: '0',
+			padding: '1rem 0.5rem',
+			display: 'flex',
+			justifyContent: 'space-between'
+		}} onClick={props.onClick}>
+			<div>
+				{props.inBascket ?
+					<></>
+					:
+					<span style={{
+						marginRight: '1rem'
+					}}>N° Items</span>
+				}
+				<span>R$ Total</span>
+			</div>
+			<Button />
+		</footer >
+	)
+}
