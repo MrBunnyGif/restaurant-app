e06073944b445be2cd886b85007b2b11677b714e
Pedro <botegapedro@gmail.com>
2023-04-10 21:03:46 -0300

    Updates BascketFooter compoennt call props

--- src/PrivateRoutes.tsx
+++ src/PrivateRoutes.tsx
@@ -1,10 +1,19 @@
 import { Navigate, Outlet } from "react-router"
 import Auth from "./Auth"
 import BascketFooter from "./components/BascketFooter"
+import { useLocation, useNavigate } from 'react-router-dom';
 
-export default () => (
-  <>
-    {Auth.isSectionOn ? <Outlet /> : <Navigate to='/' />}
-			<BascketFooter />
-  </>
-)
+export default () => {
+  const routes = useLocation()
+  const navigate = useNavigate()
+
+  return (
+    <>
+      <BascketFooter
+        inBascket={routes.pathname.includes('/item')}
+        onClick={() => routes.pathname.includes('/menu') ? navigate('/bascket') : null}
+      />
+      {Auth.isSectionOn ? <Outlet /> : <Navigate to='/' />}
+    </>
+  )
+}
